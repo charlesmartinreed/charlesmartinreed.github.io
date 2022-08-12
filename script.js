9fc64048000632f151355718bc7c2f09a707f9df
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-08-12 01:09:39 -0500

    Add modal toggling, background fade effect.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const modals =
-  document.querySelectorAll(".project-card").previousElementSibling;
+const fadeDiv = document.getElementById("fade-div");
+const projectCards = document.querySelectorAll(".project-card");
+const modals = document.querySelectorAll(".project-modal");
 const modalCloseBtn = document.querySelectorAll(".project-modal-close-btn");
 
 // modals.forEach((modal) => {
@@ -15,13 +16,31 @@ modalCloseBtn.forEach((modalBtn) =>
   })
 );
 
-function openModal(modal) {
+projectCards.forEach((projectCard) => {
+  projectCard.addEventListener("click", (e) => {
+    // if (e.target.children !== 0) {
+    //   console.log(e.target);
+    // }
+    // console.log(e.target);
+    openModal(projectCard);
+  });
+});
+
+function openModal(card) {
+  const [modal] = Array.from(modals).filter(
+    (modal) =>
+      modal.getAttribute("data-project-name") ===
+      card.getAttribute("data-project-name")
+  );
   //   modal.classList.add("visible");
-  console.log("modal opening");
+  modal.classList.add("visible");
+  fadeDiv.classList.add("faded");
+  //   console.log("modal opening");
 }
 
 function closeModal(modal) {
   modal.classList.remove("visible");
+  fadeDiv.classList.remove("faded");
 }
 
-console.log(modals);
+console.log(projectCards);
