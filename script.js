00ccce68562fe8f0544852f23104b640000042b5
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-08-12 13:34:12 -0500

    Fix layout issues on mobile landscape, add click outside modal to close.

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const projectCards = document.querySelectorAll(".project-card");
 const modals = document.querySelectorAll(".project-modal");
 const modalCloseBtn = document.querySelectorAll(".project-modal-close-btn");
 
-// modals.forEach((modal) => {
-//   modal.addEventListener("click", (e) => {
-//     // openModal(e);
-//     console.log(e.target);
-//   });
-// });
-
 modalCloseBtn.forEach((modalBtn) =>
   modalBtn.addEventListener("click", (e) => {
     closeModal(e.target.parentElement);
@@ -18,10 +11,6 @@ modalCloseBtn.forEach((modalBtn) =>
 
 projectCards.forEach((projectCard) => {
   projectCard.addEventListener("click", (e) => {
-    // if (e.target.children !== 0) {
-    //   console.log(e.target);
-    // }
-    // console.log(e.target);
     openModal(projectCard);
   });
 });
@@ -32,15 +21,13 @@ function openModal(card) {
       modal.getAttribute("data-project-name") ===
       card.getAttribute("data-project-name")
   );
-  //   modal.classList.add("visible");
   modal.classList.add("visible");
   fadeDiv.classList.add("faded");
-  //   console.log("modal opening");
+  fadeDiv.addEventListener("click", () => closeModal(modal));
 }
 
 function closeModal(modal) {
   modal.classList.remove("visible");
   fadeDiv.classList.remove("faded");
+  fadeDiv.removeEventListener("click", null);
 }
-
-console.log(projectCards);
