5ad0b6791d77b08d156578acb2b06677d3d9d4fb
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-07-13 23:05:21 -0500

    Add keyframe animation to info/close button.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,29 @@
 const infoBtns = document.querySelectorAll('.project__info__btn');
 const infoPanes = document.querySelectorAll('.project__details');
 
-infoBtns.forEach((infoBtn) => infoBtn.addEventListener('click', (e) => {
+infoBtns.forEach((infoBtn) =>
+  infoBtn.addEventListener('click', (e) => {
     let index = e.target.getAttribute('data-index');
-    let [pane] = Array.from(infoPanes).filter((infoPane, paneIdx) => paneIdx == index);
+    let [pane] = Array.from(infoPanes).filter(
+      (infoPane, paneIdx) => paneIdx == index
+    );
 
-    pane.classList.toggle('visible')
-    // console.log('index clicked', index);
-    // console.log('coresponding pane', pane);
-}))
+    e.target.textContent = '';
+    e.target.classList.toggle('info__opened');
+
+    e.target.animate(
+      { transform: 'rotateY(360deg)' },
+      { duration: 500, easing: 'cubic-bezier(0.42, 0, 0.58, 1)' }
+    );
+
+    setTimeout(() => {
+      e.target.textContent = e.target.classList.contains(
+        'info__opened'
+      )
+        ? 'X'
+        : '?';
+    }, 400);
+
+    pane.classList.toggle('visible');
+  })
+);
