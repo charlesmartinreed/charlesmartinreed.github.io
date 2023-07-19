dccdecc1cf0d74645af93db49dca823846edd66a
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-07-18 23:27:46 -0500

    Add dark/light mode toggle functionality, store user pref.

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ const infoPanes = document.querySelectorAll('.project__details');
 const darkLightModeToggle = document.querySelector(
   '#light__dark__mode__toggle'
 );
+const darkLabel = document.querySelector('#dark__mode__label__dark');
+const lightLabel = document.querySelector(
+  '#dark__mode__label__light'
+);
 
 infoBtns.forEach((infoBtn) =>
   infoBtn.addEventListener('click', (e) => {
@@ -32,20 +36,59 @@ infoBtns.forEach((infoBtn) =>
 );
 
 darkLightModeToggle.addEventListener('click', (e) => {
-  // swap current page and background colors
+  toggleDarkLightMode();
+});
+
+function toggleDarkLightMode() {
+  updateDarkLightModeValues();
+}
+
+function updateDarkLightModeValues() {
   const root = document.querySelector(':root');
   const computed = getComputedStyle(root);
 
-  let currentBGColor = computed.getPropertyValue(
+  let newBGColor = computed.getPropertyValue('--default-font-color');
+  let newTextColor = computed.getPropertyValue(
     '--default-page-background-color'
   );
-  let currentTextColor = computed.getPropertyValue(
-    '--default-font-color'
-  );
 
-  root.style.setProperty('--default-font-color', currentBGColor);
+  setDarkLightModeValues(newBGColor, newTextColor);
+  storeUserDarkModePref(newBGColor, newTextColor);
+}
+
+function setDarkLightModeValues(newBGColor, newTextColor) {
+  const root = document.querySelector(':root');
+  const computed = getComputedStyle(root);
+
+  root.style.setProperty('--default-font-color', newTextColor);
   root.style.setProperty(
     '--default-page-background-color',
-    currentTextColor
+    newBGColor
   );
+
+  darkLabel.classList.toggle('on', newBGColor == `rgb(34, 34, 34)`);
+  lightLabel.classList.toggle('on', newBGColor != `rgb(34, 34, 34)`);
+}
+
+function storeUserDarkModePref(bgColor, textColor) {
+  if (window.localStorage) {
+    window.localStorage.setItem(
+      'darkModePreference',
+      JSON.stringify({
+        bgColor: bgColor,
+        textColor: textColor,
+      })
+    );
+  }
+}
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  if (window.localStorage) {
+    if (window.localStorage.getItem('darkModePreference')) {
+      let { bgColor, textColor } = JSON.parse(
+        window.localStorage.getItem('darkModePreference')
+      );
+      setDarkLightModeValues(bgColor, textColor);
+    }
+  }
 });
